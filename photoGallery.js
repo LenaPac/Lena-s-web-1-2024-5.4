8d15edbd393b2f590bd2f1f21c193383ef0df5db
LenaPac <143618741+LenaPac@users.noreply.github.com>
2024-04-25 13:27:37 +0000

    Cutie

--- photoGallery.js
+++ photoGallery.js
@@ -1,19 +1,23 @@
 // 1. Declare a variable name 'imageContainer' and store the html element with the id 'imageContainer'
 //  - Using querySelector, store the selector #imageContainer
+console.log("dd");
 
 let imageContainer=document.querySelector("#imageContainer");
 
 console.log(imageContainer);
 
 // 2. Add at least four image urls to the imageUrls array
-let imageUrls = ["Photo1.jpg","Photo2.jpg","Photo3.jpg","Photo4.jpg"];
+let imageUrls = ["https://jonahwrites.blog/wp-content/uploads/2022/12/marin.jpg?w=1024",
+"https://i.pinimg.com/originals/97/74/d3/9774d30531ece341b2bb6f4634579988.jpg",
+"Photo3.jpg",
+"Photo4.jpg"];
 
 
 let image;
 // 3. Selecting the imageUrls array, create a forEach loop.
-imageUrls.forEach(function(image){
+imageUrls.forEach(function(i){
     image = document.createElement("img");
-    image.src =image;
+    image.src = i;
     imageContainer.appendChild(image);
 
 });
